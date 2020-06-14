ced6b65d195b9ca94af1a9af796a5469c004ad0d
ra169374 <ra169374@gitlab.ic.unicamp.br>
2020-06-14 00:43:53 -0400

    Criadas algumas funções auxiliares no programa; adicionado o esqueleto do script de teste.

--- tarefa11/frequencia.py
+++ tarefa11/frequencia.py
@@ -1,27 +1,67 @@
+"""
+Daniel Paulo Garcia © 2020
+
+frequencia.py
+
+Descrição: ???
+"""
+
 from typing import List, Tuple
 
-Texto = List[str]
+ListaDePalavras = List[str]
 
 
-def ler_entrada() -> Tuple[Texto, Texto]:
+def calcular_quartis():
+    """docstring"""
+    pass
+
+
+def separar_palavras(texto: str, stop_words: ListaDePalavras):
+    """docstring"""
+    lista_palavras = []
+    palavra = ""
+
+    for c in range(len(texto)):
+
+        if texto[c] == ' ' or texto[c] == ',':
+            if palavra not in stop_words:
+                lista_palavras.append(palavra)
+            palavra = ""
+            continue
+
+        palavra += texto[c]
+
+    return lista_palavras
+
+
+def ler_entrada() -> ListaDePalavras:
     """
     Lê duas linhas da entrada padrão, a primeira contendo o caminho do arquivo de texto a ser processado e a segunda
     contendo as stop words (palavras ignoradas pelo programa). Retorna a lista de palavras do texto ordenadas em ordem
-    alfabetica invertida e a lista de stop words na ordem em que foram fornecidas.
+    alfabetica invertida.
+
+    ATUALIZAR DOCSTRING!
     """
+
+    # Lê texto do arquivo especificado
     caminho_arquivo = input()
     arquivo_texto = open(caminho_arquivo)
-    texto = sorted(arquivo_texto.read().split())
+    texto = arquivo_texto.read()
     arquivo_texto.close()
 
+    # Lê stop-words da entrada padrão
     stop_words = input().split()
-    return texto, stop_words
+
+    lista_palavras = separar_palavras(texto, stop_words)
+
+    return lista_palavras
 
 
 def main():
-    texto, stop_words = ler_entrada()
+    palavras = ler_entrada()
 
     # Imprimir as três palavras mais frequentes, da mais à menos frequente
+    print(palavras[:2])
 
     # Imprimir número de palavras cuja frequência é maior ou igual à da última palavra do primeiro quartil, quando
     # consideramos as palavra da mais à menos frequente. Para determinar o quartil, desconsidere palavras que se repetem
---
+++ tarefa11/testar_funcao_separar_palavras.py
@@ -0,0 +1,9 @@
+from frequencia import separar_palavras
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
