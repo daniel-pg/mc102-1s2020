e7d7cbccb428fd0ce02c355e2de2d4b611da18df
ra169374 <ra169374@gitlab.ic.unicamp.br>
2020-07-15 19:23:20 -0400

    Resolução do collatz.py

--- tarefa14/collatz.py
+++ tarefa14/collatz.py
@@ -4,12 +4,29 @@ Daniel Paulo Garcia © 2020
 collatz.py
 
 Descrição: Dado um número inteiro positivo, determina o número de repetições do procedimento descrito na conjectura de
-Collatz para chegar em 1.
+Collatz (modificada na tarefa) para chegar em 1.
 """
 
 
+def calcular_collatz(n, iteracoes):
+
+    if n == 1:
+        return iteracoes
+
+    elif n % 2 == 0:
+        n //= 2
+
+    else:
+        n = (3*n + 1) // 2
+
+    iteracoes += 1
+    return calcular_collatz(n, iteracoes)
+
+
 def main():
-    pass
+    n = int(input())
+    resultado = calcular_collatz(n, 0)
+    print(resultado)
 
 
 if __name__ == '__main__':
