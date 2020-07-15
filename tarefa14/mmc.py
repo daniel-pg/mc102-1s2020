32f0622aab9841f16fe9be579d98d73d7d58d741
ra169374 <ra169374@gitlab.ic.unicamp.br>
2020-07-15 17:43:49 -0400

    Resolução do mmc.py

--- tarefa14/mmc.py
+++ tarefa14/mmc.py
@@ -7,8 +7,21 @@ Descrição: Dados dois números, a e b, calcula o mínimo múltiplo comum entre
 """
 
 
+def calcular_mdc(a, b):
+    if b == 0:
+        return a
+    else:
+        return calcular_mdc(b, a % b)
+
+
+def calcular_mmc(a, b):
+    return (a // calcular_mdc(a, b)) * b
+
+
 def main():
-    pass
+    a, b = [int(n) for n in input().split()]
+    resultado = calcular_mmc(a, b)
+    print(resultado)
 
 
 if __name__ == '__main__':
