8ac38e67629c70b8e6fd18322023e01d64c0dc51
ra169374 <ra169374@gitlab.ic.unicamp.br>
2020-07-15 23:42:38 -0400

    Resolução do hanoi.py

--- tarefa14/hanoi.py
+++ tarefa14/hanoi.py
@@ -7,8 +7,17 @@ Descrição: Determina o número mínimo de movimentos para resolver o problema 
 """
 
 
+def hanoi(n):
+    if n == 1:
+        return 1
+    else:
+        return 2 * hanoi(n - 1) + 1
+
+
 def main():
-    pass
+    n = int(input())
+    resultado = hanoi(n)
+    print(resultado)
 
 
 if __name__ == '__main__':
