679b827286373025445801fb186cfaa25f51f554
ra169374 <ra169374@gitlab.ic.unicamp.br>
2020-07-17 14:50:13 -0400

    Resolução do potencia.py

--- tarefa14/potencia.py
+++ tarefa14/potencia.py
@@ -7,8 +7,21 @@ Descrição: Dado dois números inteiros positivos a e b, calcula a ** b fazendo
 """
 
 
+def potencia(a, b):
+    if b == 0:
+        return 1
+    elif b == 1:
+        return a
+    elif b % 2 == 0:
+        return potencia(a * a, b // 2)
+    else:
+        return a * potencia(a * a, (b - 1) // 2)
+
+
 def main():
-    pass
+    a, b = [int(n) for n in input().split()]
+    resultado = potencia(a, b)
+    print(resultado)
 
 
 if __name__ == '__main__':
