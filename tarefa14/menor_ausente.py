394b10ae9d1405094aa93908aec48e76d9a92bcb
ra169374 <ra169374@gitlab.ic.unicamp.br>
2020-07-18 00:45:45 -0400

    Resolução do menor_ausente.py

--- tarefa14/menor_ausente.py
+++ tarefa14/menor_ausente.py
@@ -7,8 +7,20 @@ Descrição: Dada uma sequência ordenada (crescente) de números, encontra o me
 """
 
 
+def encontra_menor_ausente(sequencia, idx):
+    if idx < len(sequencia) - 1:
+        if sequencia[idx] == sequencia[idx + 1] - 1:
+            return encontra_menor_ausente(sequencia, idx + 1)
+        else:
+            return sequencia[idx] + 1
+    else:
+        return -1
+
+
 def main():
-    pass
+    entrada = [int(n) for n in input().split()]
+    menor_ausente = encontra_menor_ausente(entrada, 0)
+    print(menor_ausente)
 
 
 if __name__ == '__main__':
