1f9033b89978056cf3371eb94652575c69e56ddd
ra169374 <ra169374@gitlab.ic.unicamp.br>
2020-07-21 22:58:35 -0400

    Algum progresso foi feito no n-digitos.py. A saída ainda não está correta.

--- tarefa14/n-digitos.py
+++ tarefa14/n-digitos.py
@@ -8,8 +8,34 @@ todas combinações em ordem crescente.
 """
 
 
+def encontrar_combinacoes(vetor, n, s):
+    if s < 0:
+        return
+
+    elif s == 0:
+        novo_vetor = vetor + tuple((0 for _ in range(n)))
+        for c in novo_vetor:
+            print(c, end="")
+        print()
+        return novo_vetor
+
+    elif n == 1 and s <= 9:
+        novo_vetor = vetor + (s,)
+        for c in novo_vetor:
+            print(c, end="")
+        print()
+        return novo_vetor
+
+    else:
+        for k in range(s + 1):
+            novo_vetor = vetor + (k,)
+            encontrar_combinacoes(novo_vetor, n - 1, s - k)
+
+
 def main():
-    pass
+    n, s = [int(n) for n in input().split()]
+    vetor = tuple()
+    encontrar_combinacoes(vetor, n, s)
 
 
 if __name__ == '__main__':
