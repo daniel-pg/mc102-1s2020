c1da4dbb116a7860d4b0ed239f35aca7bdeb045e
ra169374 <ra169374@gitlab.ic.unicamp.br>
2020-07-22 22:30:04 -0400

    Criado o esqueleto básico da tarefa 15.

---
+++ tarefa15/arvore.py
@@ -0,0 +1,18 @@
+"""
+Daniel Paulo Garcia © 2020
+
+arvore.py
+
+Descrição: Programa que recebe o link de uma página inicial e mostra a hierarquia entre as páginas que podem ser
+alcançadas a partir da inicial.
+"""
+
+from modulo import *
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
