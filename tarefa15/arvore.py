fd5a4e6d266dbc64dad8c8711ea276c3dc19859d
ra169374 <ra169374@gitlab.ic.unicamp.br>
2020-07-23 16:55:46 -0400

    O arvore.py já consegue obter os links do HTML de forma correta, aparentemente.

--- tarefa15/arvore.py
+++ tarefa15/arvore.py
@@ -7,11 +7,36 @@ Descrição: Programa que recebe o link de uma página inicial e mostra a hierar
 alcançadas a partir da inicial.
 """
 
-from modulo import *
+# Nunca é uma boa ideia importar usando *
+from modulo import resolver_url, eh_url_valida, obter_html
+import re
+
+
+def imprimir_arvore(url: str, url_inicial: str, urls_ja_acessadas: set) -> None:
+    """
+    Dadas uma URL qualquer de um site e a URL de sua respectiva página principal, imprime recursivamente a árvore de
+    todos os links acessíveis a partir da página, desde que estejam ou no mesmo diretório ou em subdiretórios da página
+    inicial, e não sejam links externos.
+
+    :param urls_ja_acessadas: Conjunto onde serão guardadas strings contendo as URL's que ja foram listadas pela função.
+    :param url_inicial: URL da página inicial.
+    :param url: URL a partir da qual será construída a árvore.
+    :return:
+    """
+    html = obter_html(url)
+    matches = re.finditer(r'href=[\'"]([^#><@\s]+(?:\.html?|/))[\'"]', html)
+
+    for match in matches:
+        url_encontrada = resolver_url(match.group(1), url_inicial)
+        if eh_url_valida(url_encontrada, url_inicial) and url_encontrada not in urls_ja_acessadas:
+            urls_ja_acessadas.add(url_encontrada)
+            print(match.group(1))
 
 
 def main():
-    pass
+    pagina_inicial = input()
+    urls_ja_acessadas = set()
+    imprimir_arvore(pagina_inicial, pagina_inicial, urls_ja_acessadas)
 
 
 if __name__ == '__main__':
